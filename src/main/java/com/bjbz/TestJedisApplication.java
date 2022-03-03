ddf72431d000e73eb63bb7abda83bdba6c1e7ca6
Lhh <2078854064@qq.com>
2022-03-03 08:53:57 +0800

    third commit

--- src/main/java/com/bjbz/TestJedisApplication.java
+++ src/main/java/com/bjbz/TestJedisApplication.java
@@ -9,6 +9,7 @@ public class TestJedisApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestJedisApplication.class, args);
         System.out.println("运行成功！");
+        System.out.println("hello git3！");
     }
 
 }
