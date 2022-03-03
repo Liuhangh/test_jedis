7331a4ddaf5f5fa2d908399ccfa2541703ddc41e
Lhh <2078854064@qq.com>
2022-03-03 08:52:20 +0800

    second commit

--- src/main/java/com/bjbz/TestJedisApplication.java
+++ src/main/java/com/bjbz/TestJedisApplication.java
@@ -8,6 +8,7 @@ public class TestJedisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestJedisApplication.class, args);
+        System.out.println("运行成功！");
     }
 
 }
